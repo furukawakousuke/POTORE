03b2d4821fba085f2075c2ce80405439d5b64636
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2023-02-12 08:24:59 +0000

    2023/02/12

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :admins, skip: [:registrations, :passwords], controllers:{
   sessions: 'admin/sessions'
-}
+  }
 
   devise_scope :posters do
     post 'posters/guest_sign_in' , to: 'posters/sessions#guest_sign_in'
     post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
   end
+  
   devise_for :posters,controllers:{
-    registrations: 'public/registrations',
-  sessions: 'public/sessions'
-  }
+  registrations: 'public/registrations',
+  sessions: 'public/sessions'}
 
 
   namespace :admin do
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     get 'sending/:id' => 'reports#sending',as: 'sending'
     resources :reports, only: [:index,:show,:update,:destroy]
   end
+  
   scope module: :public do
     resources :notifications, only: [:index] do
       collection do
@@ -27,23 +28,21 @@ Rails.application.routes.draw do
       end
     end
     resources :post_photos, only: [:new,:index,:show,:edit,:create,:update,:destroy] do
-      get :search, on: :collection
-      resource :favorites, only: [:create,:destroy]
-      resources :comments, only: [:create,:destroy]
-    resources :reports, only: [:new,:create]
+     get :search, on: :collection
+     resource :favorites, only: [:create,:destroy]
+     resources :comments, only: [:create,:destroy]
+     resources :reports, only: [:new,:create]
     end
     resources :posters, only: [:index,:show,:edit,:update] do
-    get :search, on: :collection
-      resource :favorites, only: [:create,:destroy]
-    resource :relationships, only: [:create,:destroy]
-    get 'followings' => 'relationships#followings', as: 'follow'
-    get 'followers' => 'relationships#followers', as: 'follower'
-      member do
-       get :favorites
-      end
+     get :search, on: :collection
+     resource :favorites, only: [:create,:destroy]
+     resource :relationships, only: [:create,:destroy]
+     get 'followings' => 'relationships#followings', as: 'follow'
+     get 'followers' => 'relationships#followers', as: 'follower'
+     member do
+      get :favorites
+     end
     end
-
   end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
